8cce7dfc552ba53e7df6dad89b803db9bdc8dfb0
tanaka-k <k.tanaka@ejworks.com>
2021-05-19 02:18:40 +0900

    50.変数を使ってHTMLをカスタマイズ

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,19 @@ module.exports = {
                     //     }
                     // },
                 ]
+            },{
+                test: /\.pug/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true,
+                        }
+                    }
+                ]
             },
         ],
     },
@@ -45,7 +58,12 @@ module.exports = {
             filename: './stylesheets/main.css',
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html'
+            template: './src/templates/index.pug',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html'
         }),
         new CleanWebpackPlugin(),
     ],
